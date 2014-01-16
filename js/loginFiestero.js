b52525df7363703ad0dddbbb34a1e75466fbedcf
Saskya Mosquera <saskya1991@gmail.com>
2014-01-16 01:14:55 +0100

    Login fiestero (parece que funciona)

--- js/loginFiestero.js
+++ js/loginFiestero.js
@@ -1,27 +1,32 @@
 function loginFiestero(){
-	var userName = document.getElementById('inputEmail');
-	var userPassword = document.getElementById('inputPassword');
-	if (userName == ""){
+	console.log("mierda");
+	var email = document.getElementById('inputEmail').value;
+	var password = document.getElementById('inputPassword').value;
+	if (email == ""){
 		alert("Se debe introcucir un nombre de usuario");
-	}else if ( userPassWord == ""){
+	}else if ( password == ""){
 		alert("Se debe introducir una contraseña");
-	}else login();
+	}else login(email,password);
 }
 
-function login(){
+function login(email2,password2){
 	$.ajax({
 			url: "http://where2night.es/login.php",
 			dataType: "json",
-			type: POST,
+			type: "POST",
+			timeout: 5000,
 			data: {
-				userName: $('#inputEmail'),
-				userPassword: $('#userPassWord')
+				email:email2,
+				password: password2
 			},
-			success:  function(e,val){
-				document.URL = "www.google.com";
-			},
+			
+			success: redirect,
 			onerror: function(e,val){
 				alert("Hay error");
 			}
 	});
+}
+
+function redirect(){
+	console.log("hola");
 }
