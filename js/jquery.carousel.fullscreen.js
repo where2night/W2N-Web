29787c4c48381150b895b897090e3d428ac35c95
Yuleska Perez <yuleskaperez@gmail.com>
2014-01-24 16:41:11 +0100

    Subida Página Principal definitiva

--- js/jquery.carousel.fullscreen.js
+++ js/jquery.carousel.fullscreen.js
@@ -9,11 +9,11 @@ jQuery(document).ready(function() {
     	interval: 4000
 	});
 
-	$('.carousel').css({'margin': 0, 'width': $(window).outerWidth(), 'height': $(window).outerHeight()});
+	$('.carousel').css({'margin':0, 'width': $(window).outerWidth(), 'height': $(window).outerHeight()});
 	$('.carousel .item').css({'position': 'fixed', 'width': '100%', 'height': '100%'});
 	$('.carousel-inner div.item img').each(function() {
 		var imgSrc = $(this).attr('src');
-		$(this).parent().css({'background': 'url('+imgSrc+') center center no-repeat', '-webkit-background-size': '100% ', '-moz-background-size': '100%', '-o-background-size': '100%', 'background-size': '100%', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
+		$(this).parent().css({'background': 'url('+imgSrc+') no-repeat center center fixed', '-webkit-background-size': '100% ', '-moz-background-size': '100%', '-o-background-size': '100%', 'background-size': '100%', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
 		$(this).remove();
 	});
 
