af36cbe6634c40ca36fddd48761316e2f717e3c8
leyamcir <leyamcir@gmail.com>
2014-03-17 22:09:43 +0100

    Separados sidebar y navbar-header del código (incluidos con php).

---
+++ user/templates/sidebar.php
@@ -0,0 +1,74 @@
+<!-- SideBar -->
+<div class="sidebar-background">
+  <div class="primary-sidebar-background"></div>
+</div>
+<div class="primary-sidebar">
+  <ul class="nav navbar-collapse collapse navbar-collapse-primary">
+       
+         <li class="">
+          <span class="glow"></span>
+          <a href="profile.php">
+              <i class=""><img class="menu-avatar" src="../images/profile.jpg" /></i>
+              <span>Mi Perfil</span>
+          </a>
+        </li>
+         <li class="">
+          <span class="glow"></span>
+          <a href="events.php">
+              <i class=""><img class="menu-avatar" src="../images/party3.jpg" /></i>
+              <span>Eventos</span>
+          </a>
+        </li>
+         <li class="">
+          <span class="glow"></span>
+          <a href="photos.php">
+              <i class=""><img class="menu-avatar" src="../images/party2.jpg" /></i>
+              <span>Fotos </span>
+          </a>
+        </li>
+         <li class="">
+          <span class="glow"></span>
+          <a href="">
+              <i class=""><img class="menu-avatar" src="../images/party4.jpg" /></i>
+              <span>Amigos</span>
+          </a>
+        </li>
+         <li class="">
+          <span class="glow"></span>
+          <a href="">
+              <i class=""><img class="menu-avatar" src="../images/party2.jpg" /></i>
+              <span>Locales</span>
+          </a>
+        </li>
+         <li class="">
+          <span class="glow"></span>
+          <a href="">
+              <i class=""><img class="menu-avatar" src="../images/party3.jpg" /></i>
+              <span>DJ's</span>
+          </a>
+        </li> 
+        <li class="">
+         <span class="glow"></span>
+          <a class="accordion-toggle collapsed " data-toggle="collapse" href="#tnnmk7rjLZ">
+              <i class=""><img class="menu-avatar" src="../images/party2.jpg" /></i>
+                    <span>
+                      Configuración&nbsp;&nbsp;&nbsp;
+                      <i class="glyphicon glyphicon-circle-arrow-down"></i>
+                    </span>
+          </a>
+          <ul id="tnnmk7rjLZ" class="collapse "> 
+               
+                  <a href="edit.php" style="font-size:12px ;color:#6C6C6C" onmouseout="javascript:this.style.color='#6C6C6C';"onmouseover="javascript:this.style.color='#F2A116';" >
+                      <i class="glyphicon glyphicon-edit" style="color:#FF6B24"></i> Editar Perfil
+                  </a>
+                <br>
+                
+                  <a href="" style="font-size:12px ;color:#6C6C6C" onmouseout="javascript:this.style.color='#6C6C6C';"onmouseover="javascript:this.style.color='#F2A116';">
+                      <i class="glyphicon glyphicon-lock"style="color:#FF6B24"></i> Privacidad
+                  </a>
+                
+          </ul>
+        </li>
+		<img src="../images/logo3_opt.png" />
+</div>
+<!-- /SideBar -->
