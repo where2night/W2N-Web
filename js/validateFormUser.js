fabec9d443dec57b8a2e4b96222e4a795a026869
Saskya Mosquera <saskya1991@gmail.com>
2014-04-05 21:28:15 +0200

    Iniciar sesión después de registrarse en fiestero close #62

--- js/validateFormUser.js
+++ js/validateFormUser.js
@@ -105,7 +105,12 @@ function validateForm() {
 						var json = JSON.parse(r.responseText);
 
 						if (json.Token != 0) {
-							redirectHomeUser();
+							var id = json.id;
+							var token = json.Token;
+							var user_type = json.type;
+							var login_type = 'normal';
+							
+							set_session_info(id, token, user_type, login_type);
 						} else
 							console.log("error registro");
 					},
@@ -352,6 +357,173 @@ function showInvalidMessage() {
 			console.log("toast is closed ...");
 		}
 	});
+}
+
+function set_session_info(id, token, user_type, login_type){
+	var url1 = "../develop/update/";
+	var params = "/" + id + "/" + token + "/" + id;
+	
+	switch(user_type){
+		case '0'://User
+		  url1 += "user.php";
+		  url1 += params;
+		  $.ajax({
+			url: url1,
+			dataType: "json",
+			type: "GET",
+			timeout: 5000,
+			complete: function(r2){
+				var json = JSON.parse(r2.responseText);
+				var picture = json.picture;
+				var name = json.name;
+				var surnames = json.surnames;
+				var birthdate = json.birthdate;
+				var gender = json.gender;
+				var music = json.music;
+				var civil_state = json.civil_state;
+				var city = json.city;
+				var drink = json.drink;
+				var about = json.about;
+				$.post("../framework/session_start.php",
+				  {
+				  	user_type: 'user',
+				    type_login: login_type,
+				    id_user: id,
+				    token: token,
+					picture: picture,
+					name: name,
+					surnames: surnames,
+					birthdate: birthdate,
+					gender: gender,
+					music: music,
+					civil_state: civil_state,
+					city: city,
+					drink: drink,
+					about: about
+				  },
+				  function(data,status){
+					//alert("Data: " + data + "\nStatus: " + status);
+					window.location.href = "../user/home.php";										  
+				  });
+	    		},
+				onerror: function(e,val){
+					alert("Contraseña y/o usuario incorrectos");
+				}
+			});
+		break;
+		case '1'://Club
+		  url1 += "local.php";
+		  url1 += params;
+		  $.ajax({
+			url: url1,
+			dataType: "json",
+			type: "GET",
+			timeout: 5000,
+			complete: function(r2){
+				var json = JSON.parse(r2.responseText);
+				var companyName = json.companyName;
+				var localName = json.localName;
+				var cif = json.cif;
+				var poblationLocal = json.poblationLocal;
+				var cpLocal = json.cpLocal;
+				var telephoneLocal = json.telephoneLocal;
+				var street = json.street;
+				var streetName = json.streetNameLocal;
+				var streetNumber = json.streetNumberLocal;
+				var music = json.music;
+				var entryPrice = json.entryPrice;
+				var drinkPrice = json.drinkPrice;
+				var openingHours = json.openingHours;
+				var closeHours = json.closeHours;
+				var picture = json.picture;
+				var about = json.about;
+				var latitude = json.latitude;
+				var longitude = json.longitude;
+				$.post("../framework/session_start.php",
+				  {
+				  	user_type: 'club',
+				    type_login: 'normal',
+				    id_user: id,
+				    token: token,
+					companyName: companyName,
+					localName: localName,
+					cif: cif,
+					poblationLocal: poblationLocal,
+					cpLocal: cpLocal,
+					telephoneLocal: telephoneLocal,
+					street: street,
+					streetName: streetName,
+					streetNumber: streetNumber,
+					music: music,
+					entryPrice: entryPrice,
+					drinkPrice: drinkPrice,
+					openingHours: openingHours,
+					closeHours: closeHours,
+					picture: picture,
+					about: about,
+					latitude: latitude,
+					longitude: longitude
+				  },
+				  function(data,status){
+					//alert("Data: " + data + "\nStatus: " + status);
+					window.location.href = "../club/home.php";										  
+				  });
+	    		},
+				onerror: function(e,val){
+					alert("Contraseña y/o usuario incorrectos");
+				}
+			});
+		break;
+		case '-1'://DJ
+		  url1 += "dj.php";
+		  url1 += params;
+		  $.ajax({
+			url: url1,
+			dataType: "json",
+			type: "GET",
+			timeout: 5000,
+			complete: function(r2){
+				var json = JSON.parse(r2.responseText);
+				var nameDJ = json.nameDJ;
+				var name = json.name;
+				var surname = json.surname;
+				var telephone = json.telephoneDJ;
+				var gender = json.gender;
+				var birthdate = json.birthdate;
+				var picture = json.picture;
+				var music = json.music;
+				var about = json.about;
+				$.post("../framework/session_start.php",
+				  {
+				  	user_type: 'dj',
+				    type_login: 'normal',
+				    id_user: id,
+				    token: token,
+					nameDJ: nameDJ,
+					name: name,
+					surname: surname,
+					telephone: telephone,
+					gender: gender,
+					birthdate: birthdate,
+					picture: picture,
+					music: music,
+					about: about
+				  },
+				  function(data,status){
+					//alert("Data: " + data + "\nStatus: " + status);
+					window.location.href = "../dj/home.php";										  ;
+				  });
+	    		},
+				onerror: function(e,val){
+					alert("Contraseña y/o usuario incorrectos");
+				}
+			});
+		break;
+		default:
+		 //ERROR
+		 alert('Login error');
+		break;
+	}
 }
 
 function redirectHomeUser() {
