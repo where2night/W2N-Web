c2dfa5e7c62b2c211fb1ef7da221dae4f2fa95ae
JaviAlejos <javieralejos89@gmail.com>
2014-04-22 16:14:21 +0200

    function follow
    return friendship visit mode. 0 if have a friendship request, 1 if is
    your friend , 2 if isn´t your friend

--- js/followfriend.js
+++ js/followfriend.js
@@ -1,10 +1,14 @@
 function paintButton(){
 	
-if((ide==id_abs)){
+
+if(!(ide==id_abs)){
+	
+	var status=follow();
+	
 	
 	//if(follow()){
 		
-		//document.write("<input id='btn01'  class='botonseguir' type='button'value='SIGUIENDO'onClick='changeMyClassName(this);'>");  
+		//document.write("<input id='btn01'  class='btn btn-success botonseguir' type='button'value='Fiestero Agregado'onClick='changeMyClassName(this);'style='background-color:#000;border-color:#ff6b24;color:#34d1be;text-shadow:none;'>");  
 		//document.getElementById('btn01').className='myClickedButton';
 	
 	//}else
@@ -24,7 +28,7 @@ if((ide==id_abs)){
 function changeMyClassName(theSeguirBtn)
 {
 	
-	if((ide==id_abs)){
+	if(!(ide==id_abs)){
 	
 			myButtonID = theSeguirBtn.id;
 			if(document.getElementById(myButtonID).className=='myClickedSeguir')
@@ -53,7 +57,7 @@ var params = "/" ;
 	params=params.concat("/");
 	params=params.concat(tok);
 	params=params.concat("/");
-	params=params.concat("1");//must be id_abs
+	params=params.concat(id_abs);
 
 
 	  
@@ -84,7 +88,7 @@ var params = "/" ;
 	params=params.concat("/");
 	params=params.concat(tok);
 	params=params.concat("/");
-	params=params.concat("1");// must be id_abs 
+	params=params.concat(id_abs);
 
 	  
 var url="../develop/actions/followFriend.php";
@@ -104,5 +108,91 @@ var url="../develop/actions/followFriend.php";
 	});
 
 
+
+}
+
+
+
+function follow(){
+
+var followers=2;
+	
+var params = "/" ;
+	params=params.concat(ide); 
+	params=params.concat("/");
+	params=params.concat(tok);
+	
+
+	  
+var url="../develop/read/myFriends.php";
+	url=url.concat(params);
+	
+	
+	$.ajax({
+			url: url,
+			dataType: "json",
+			type: "GET",
+			async: false,
+			complete: function(r){
+			  var json = JSON.parse(r.responseText);
+			  
+           var count=json.numFriends;
+	   		
+	   		var i=0;
+			 
+			 while (i<count){
+			 	
+			 	if(json[i].idProfile==id_abs)followers=1;
+			    
+			    i=i+1;
+			 }
+	
+			  
+			},
+			onerror: function(e,val){
+				alert("No se pueden saber los seguidores");
+			}
+	});
+
+
+    if (followers==2){
+    	
+	var url="../develop/read/petFriendship.php";
+	url=url.concat(params);
+
+
+	$.ajax({
+			url: url,
+			dataType: "json",
+			type: "GET",
+			async: false,
+			complete: function(r){
+			  var json = JSON.parse(r.responseText);
+			 
+           var count=json.numPetitions;
+	   		
+	   		var i=0;
+			 
+			 while (i<count){
+			 	
+			 	if(json[i].idProfile==id_abs)followers=0;
+			    
+			    i=i+1;
+			 }
+	
+			  
+			},
+			onerror: function(e,val){
+				alert("No se pueden saber los seguidores");
+			}
+	});
+    	
+    	
+    	
+    }
+
+
+
+return followers;
 
 }
