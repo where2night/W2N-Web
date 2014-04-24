c14fe786a29b6c96e12980b0b64b241e6209d70d
Yuleska Perez <yuleskaperez@gmail.com>
2014-04-24 22:26:06 +0200

    Events user button problems

--- user/events.php
+++ user/events.php
@@ -43,15 +43,112 @@ include_once "../framework/visits.php";
 	<script src="../js/moment-with-langs.js"></script>
 	<script src="../js/moment.min.js"></script>
 	
-
 <script type="text/javascript">
+function getVisitorEvents(){
+
+	var params = "/" ;
+	params = params.concat(ide); 
+	params = params.concat("/");
+	params = params.concat(tok);
+	  
+	var url = "../develop/actions/myEvents.php";
+	url = url.concat(params);
+	var array_ids = new Array(); 
+	$.ajax({
+			url:url,
+			dataType: "json",
+			type: "GET",
+			async: false,
+			complete: function(r){
+				var json = JSON.parse(r.responseText);	
+				
+			  	for (var i = 0; i < json.rows; i++) {
+			  		array_ids[i] = "" +json[i].idEvent;
+		  		}
+			},
+			onerror: function(e,val){
+				alert("Error al buscar eventos de usuario");
+			}
+	});
+	return array_ids;
+}
+
+
+//Check if function is join or disjoin
+function clickedJoinEvent(idRequest, idEvent){
+	var partierEvents = new Array();
+    if(ide != idRequest){
+    	partierEvents = getVisitorEvents();
+    } 
+    if(partierEvents.indexOf(idEvent) > -1){//Event in array, display disjoin
+		disjoinEvent (idRequest, idEvent)
+	}else{//Event not in array, display join
+		joinEvent (idRequest, idEvent)
+	}
+}
+
+function joinEvent (idRequest, idEvent){
+
+	var params = "/" ;
+	params = params.concat(ide); 
+	params = params.concat("/");
+	params = params.concat(tok);
+	params = params.concat("/");
+	params = params.concat(idEvent);
+	  
+	var url = "../develop/actions/goToEvent.php";
+	url = url.concat(params);
+	$.ajax({
+			url:url,
+			dataType: "json",
+			type: "GET",
+			complete: function(r){
+			 	var json = JSON.parse(r.responseText);	 
+          		var button = document.getElementById("join-event-" + idEvent);
+		  		button.innerHTML ='Me Desapunto';
+			  
+			},
+			onerror: function(e,val){
+				alert("No se puedo añadir al evento");
+			}
+	});
+}
+
+
+function disjoinEvent (idRequest, idEvent){
+
+	var params = "/" ;
+	params = params.concat(ide); 
+	params = params.concat("/");
+	params = params.concat(tok);
+	params = params.concat("/");
+	params = params.concat(idEvent);
+	  
+	var url = "../develop/actions/goToEvent.php";
+	url = url.concat(params);
+	$.ajax({
+			url:url,
+			dataType: "json",
+			type: "DELETE",
+			complete: function(r){
+			 	var json = JSON.parse(r.responseText);
+          		var button = document.getElementById("join-event-" + idEvent);
+		  		button.innerHTML ='Me Apunto';
+			  
+			},
+			onerror: function(e,val){
+				alert("No se puedo eliminar del evento");
+			}
+	});
+}
+
 function myEvents(){
 	
 	var idProfile = <?php echo $_SESSION['id_user']; ?>	;
 	var token =  "<?php echo $_SESSION['token']; ?>";
 	var params = "/" + idProfile + "/" + token;
-	
-var url2="../develop/actions/myEvents.php";
+
+	var url2="../develop/actions/myEvents.php";
 	url2=url2.concat(params);
 
 	$.ajax({
@@ -63,7 +160,7 @@ var url2="../develop/actions/myEvents.php";
 			var rows = json.rows;
 			for(var i=0; i<rows; i++){
 				var events=document.getElementById('myEvents').innerHTML;
-				var picture = json[i].picture;
+				var picture = json[i].pictureC;
 				var dateEvent = json[i].date; 
 				var year = dateEvent.substring(0,4);
 				var month = dateEvent.substring(5,7);
@@ -104,7 +201,7 @@ var url2="../develop/actions/myEvents.php";
 
 				if (picture==0 || picture=="" || picture==null)
 					picture = "../images/reg2.jpg";
-					events = events.concat("<li><div class='workflow-item hover' style='background-image:url(../images/reg2.jpg);background-size:100% 100%'></div>");
+					events = events.concat("<li><div class='workflow-item hover' style='background-image:url("+picture+");background-size:100% 100%'></div>");
 					events = events.concat("<span name='localName' class='label label-dark-blue' style='font-size:12px'>Evento Local</span> "+localName+" ");
 					events = events.concat("<span style='font-size:12px;color:orange'>  publicado <i class='glyphicon glyphicon-time'style='color:#FF6B24;font-size:12px'></i> "+activityFromNow+"</span></li>");
 					events = events.concat("<table class='table  tablaC1'><tbody><tr><td><h5 style='color:#ff6b24'>Título Evento <b style='color:orange'>'");
@@ -157,181 +254,39 @@ var params = "/" ;
 					alert("Contraseña y/o usuario incorrectos");
 				}
 			});
-
-
-
-	myEvents();
-	
-	
+	myEvents();	
 }
-
-var actual_page = 0;
-function showMore(){
-	/*Increases the page where I go*/
-	actual_page = actual_page +1;
-
-	/*prepares attributes for the server*/
-	var idProfile = <?php echo $_SESSION['id_user']; ?>;
-	var token =  "<?php echo $_SESSION['token']; ?>	";
-	var params = "/" + idProfile + "/" + token;
-	var page = 0;
-	var url1 = "../develop/read/news.php" + params+"/"+actual_page;
-	/*Sent by the server url page we now load, besides the token and identifier*/
-	/*the query is made ​​to the server, via GET*/
-	$.ajax({
-
-		url: url1,
-		dataType: "json",
-		type: "GET",
-		timeout: 5000,
-		
-		complete: function(r2){
-			/*In response the server a list of 10 activities is received*/
-			var json = JSON.parse(r2.responseText);
-			var num_elements = json.numElems;
-			var num_page = Math.floor(num_elements/10);
-			
-			/*Calculates if you follow the link to appear "show more"*/
-			if (num_page == actual_page){
-				var s = document.getElementById('show_more');
-				var div = document.getElementById('show_more');
-
-				div.innerHTML = '';
-			}
-			
-			/*Calculate the maximum number of items you can go to the list*/
-			var len = num_elements - actual_page*10;
-			var max_elemts;
-			
-			if (len > 10){
-				max_elemts = 10;
-			}else if (len < 10){
-				max_elemts = len;
-			}
-			
-			/*Browse the list received*/
-			for(var i=0; i<max_elemts; i++){
-				/*The list that is returned is the activity that can be 5 types:
-					1.- Created by local events that follow   --> the type we want here
-					2.- Friends we follow, change status
-					3.- Friends we follow, change their mode(De tranquis,Hoy no me lio,Lo que surja,Lo daré todo,Destroyer,Yo me llamo Ralph)
-					4.- Friend add to favorites a local
-					5.- 5.- Events friends attending */
-				var type = json[i].TYPE;
-				var goes = json[i].GOES;
-				var date = json[i].createdTime;
-				var picture = json[i].picture;
-				
-				/*Calculates uptime*/
-
-				moment.lang('es', {
-					relativeTime : {
-					future : "en %s",
-					past : "hace %s",
-					s : "unos segundos",
-					m : "un minuto",
-					mm : "%d minutos",
-					h : "una hora",
-					hh : "%d horas",
-					d : "un día",
-					dd : "%d días",
-					M : "un mes",
-					MM : "%d meses",
-					y : "un año",
-					yy : "%d años"
-					}
-				});
-				var dateActivity = moment(date);
-				if (dateActivity.isValid()){
-				var activityFromNow = dateActivity.fromNow();
-				}
-
-				/*must distinguish what type of activity relates (mentioned above), to paint in the html in the right way*/
-				if (type == 1){
-					/*1.- Created by local events that follow*/
-					var localName = json[i].localName;
-					var title = json[i].title;
-					var startHour = json[i].startHour;
-					var closeHour = json[i].closeHour;
-
-					var id_local =  json[i].idProfileLocal;
-					var link = "../club/profile.php?idv=" + id_local;
-					var streetNameLocal = json[i].streetNameLocal;
-					var streetNumberLocal = json[i].streetNumberLocal;
-					var text = json[i].text;
-					if (picture==0 || picture=="" || picture==null)
-					picture = "../images/reg2.jpg";
-					
-					$('#test').append('<li class=""> <div class="workflow-item hover" style=" background-image:url('+picture+');background-size:100% 100%"></div> <span class="label label-dark-blue" style="font-size:12px;">Evento Local</span> <a href="'+ link +'">'+ localName +'</a> <span style="font-size:12px;color:orange"> Publicado <i class="glyphicon glyphicon-time"style="color:#FF6B24;font-size:12px;"></i>'+activityFromNow+'</span></li><table class="table  tablaC1"><tbody><tr class=""><td><h5 style="color:#ff6b24"> '+title+'</h5><p style="color:#707070;font-size:14px;"></p><p style="color:#E5E4E2;font-size:14px;">'+text+'</p><input id="btn01"  class="btn btn-success botonapuntar " type="button"value="Me Apunto"onClick="btnApuntar(this);"style="background-color:#000;border-color:#ff6b24;color:#34d1be;text-shadow:none;"></td></tr></tbody></table>');
-				}
-			}
-
-		},
-	
-		onerror: function(e,val){
-		alert("Problema al mostrar eventos");
-		}
-	});
-
-}
-	$(document).ready(function(){
-
-	//Get user info
-	var idProfile = <?php echo $_SESSION['id_user']; ?>	;
-	var token =  "<?php echo $_SESSION['token']; ?>	";
-	var params = "/" + idProfile + "/" + token;
-	var url2 = "../develop/update/partier.php" + params;
-
-	$.ajax({
-	url:url2,
-	dataType: "json",
-	type: "GET",
-	complete: function(r3){
-	var json = JSON.parse(r3.responseText);
-	var name = json.name;
-	var surnames = json.surnames;
-	$("#navbar-complete-name").text(name + " " + surnames);
-	$("#navbar-complete-name2").text(name + " " + surnames);
-	},
-	onerror: function(e,val){
-	alert("No se puede introducir evento 2");
-	}
-	});
-
-	/*prepares attributes for the server*/
+function allEvents(){
 	var idProfile = <?php echo $_SESSION['id_user']; ?>	;
 	var token =  "<?php echo $_SESSION['token']; ?>";
 	var params = "/" + idProfile + "/" + token;
-	var page = 0;
-	var url1 = "../develop/read/news.php" + params+"/"+page;
 
-	/*Sent by the server url page we now load, besides the token and identifier*/
-	/*the query is made ​​to the server, via GET*/
+	var url2="../develop/actions/eventsDontGo.php";
+	url2=url2.concat(params);
 	$.ajax({
-
-		url: url1,
+		url:url2,
 		dataType: "json",
 		type: "GET",
-		timeout: 5000,
-		complete: function(r2){
-			/*In response the server a list of 10 activities is received*/
-			var json = JSON.parse(r2.responseText);
-			var num_elements = json.numElems;
+		complete: function(r3){
+		
+			var json = JSON.parse(r3.responseText);
+			var rows = json.rows;
+			/*var partierEvents;
+            if(ide != idRequest){
+            	partierEvents = getVisitorEvents();
+            } */
+            
+			for(var i=0; i<rows; i++){
 			
-			/*Browse the list received*/
-			for(var i=0; i<10; i++){
-				/*The list that is returned is the activity that can be 5 types:
-					1.- Created by local events that follow
-					2.- Friends we follow, change status
-					3.- Friends we follow, change their mode(De tranquis,Hoy no me lio,Lo que surja,Lo daré todo,Destroyer,Yo me llamo Ralph)
-					4.- Friend add to favorites a local
-					5.- Events friends attending */
-					
-				var type = json[i].TYPE;
-				var goes = json[i].GOES;
-				var date = json[i].createdTime;
-				var picture = json[i].picture;
+				var events=document.getElementById('allEvents').innerHTML;
 				
+				var picture = json[i].pictureC;
+				var dateEvent = json[i].date; 
+				var year = dateEvent.substring(0,4);
+				var month = dateEvent.substring(5,7);
+				var day = dateEvent.substring(8,11);
+				var eventDate = day+'-'+month+'-'+year;
+				var date = json[i].createdTime;
 				/*Calculates uptime*/
 				moment.lang('es', {
 					relativeTime : {
@@ -354,37 +309,51 @@ function showMore(){
 				if (dateActivity.isValid()){
 					var activityFromNow = dateActivity.fromNow();
 				}
-
-				/*must distinguish what type of activity relates (mentioned above), to paint in the html in the right way*/
-				if (type == 1){
-					/*1.- Created by local events that follow*/
-					var localName = json[i].localName;
+					
 					var title = json[i].title;
 					var startHour = json[i].startHour;
+					var starH = startHour.substring(0,5);
 					var closeHour = json[i].closeHour;
-
-					var id_local =  json[i].idProfileLocal;
-					var link = "../club/profile.php?idv=" + id_local;
-					var streetNameLocal = json[i].streetNameLocal;
-					var streetNumberLocal = json[i].streetNumberLocal;
+					var closeH = closeHour.substring(0,5);
 					var text = json[i].text;
+					var localName = json[i].name;
+					var idEvent = json[i].idEvent;
 
-					if (picture==0 || picture=="" || picture==null)
+				if (picture==0 || picture=="" || picture==null)
 					picture = "../images/reg2.jpg";
-					$('#test').append('<li class=""> <div class="workflow-item hover" style=" background-image:url('+picture+');background-size:100% 100%"></div> <span class="label label-dark-blue" style="font-size:12px;">Evento Local</span> <a href="'+ link +'">'+ localName +'</a> <span style="font-size:12px;color:orange"> Publicado <i class="glyphicon glyphicon-time"style="color:#FF6B24;font-size:12px;"></i>'+activityFromNow+'</span></li><table class="table  tablaC1"><tbody><tr class=""><td><h5 style="color:#ff6b24"> '+title+'</h5><p style="color:#707070;font-size:14px;"></p><p style="color:#E5E4E2;font-size:14px;">'+text+'</p><input id="btn01"  class="btn btn-success botonapuntar " type="button"value="Me Apunto"onClick="btnApuntar(this);"style="background-color:#000;border-color:#ff6b24;color:#34d1be;text-shadow:none;"></td></tr></tbody></table>');
-
-				}
+					events = events.concat("<li><div class='workflow-item hover' style='background-image:url("+picture+");background-size:100% 100%'></div>");
+					events = events.concat("<span name='localName' class='label label-dark-blue' style='font-size:12px'>Evento Local</span> "+localName+" ");
+					events = events.concat("<span style='font-size:12px;color:orange'>  publicado <i class='glyphicon glyphicon-time'style='color:#FF6B24;font-size:12px'></i> "+activityFromNow+"</span></li>");
+					events = events.concat("<table class='table  tablaC1'><tbody><tr><td><h5 style='color:#ff6b24'>Título Evento <b style='color:orange'>'");
+					events = events.concat(json[i].title);
+					events = events.concat("'</b></h5><p style='color:#707070;font-size:14px;margin-left:12%; '>");
+					events = events.concat(json[i].text);
+					events = events.concat("</P>");
+					events = events.concat("<p style='color:#ff6b24'>Fecha : <b style='color:#34d1be'> ");
+					events = events.concat(eventDate);
+					events = events.concat("</b>, a partir de  <b style='color:#34d1be'>");
+					events = events.concat(starH);
+					events = events.concat("</b> hasta  <b style='color:#34d1be'>");
+					events = events.concat(closeH);
+					events = events.concat("</b> hrs.</p>");
+					//BOTON//
+					/*if(partierEvents.indexOf(json[i].idEvent) > -1){//Event in array, display disjoin
+							events=events.concat('<button type="button" id="join-event-' + json[i].idEvent + '" class="btn pull-right" style="margin-right:5%;background-color:#000;border-color:#ff6b24;color:#34d1be;text-shadow:none;" onclick="clickedJoinEvent(' + "'" + idRequest + "'" + ', ' + "'" + json[i].idEvent + "'" + ');">Me Desapunto</button>');
+						}else{//Event not in array, display join
+							events=events.concat('<button type="button" id="join-event-' + json[i].idEvent + '" class="btn pull-right" style="margin-right:5%;background-color:#000;border-color:#ff6b24;color:#34d1be;text-shadow:none;" onclick="clickedJoinEvent(' + "'" + idRequest + "'" + ', ' + "'" + json[i].idEvent + "'" + ');">Me Apunto</button>');
+						}*/
+					//FIN BOTON
+					events = events.concat("</td></tr></tbody></table>");
+					
+					document.getElementById('allEvents').innerHTML=events;			   
 			}
 
 		},
 		onerror: function(e,val){
-		alert("Contraseña y/o usuario incorrectos");
+		alert("No se puede introducir evento 2");
 		}
-	});
-	
-	$('#show_more').append('<a href="home.php#both" onclick="showMore();">Mostrar más..</a>	');
-
-});//end $(document).ready(function()
+		});
+}
 </script>
 </head>
 
@@ -469,8 +438,10 @@ var tok = '<?php echo $toke; ?>' ;
 										<!-- Comienza Todos los Eventos -->
 											<div class="tab-pane fade " id="tab-allEvents">
 												 <div class="the-timeline">
-														<ul id="test">
-
+														<ul id="allEvents">
+															<script>
+																allEvents();
+															</script>
 														</ul>
 
 													</div>
